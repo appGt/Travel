7ea3094ea626ab1b206fdcef055319c6dea69c66
曾志勇 <zengzhiyong@jiaxincloud.com>
2018-07-16 15:49:35 +0800

    添加搜索路由

--- src/pages/home/components/header.vue
+++ src/pages/home/components/header.vue
@@ -6,10 +6,12 @@
     <div class="header-input">
       <span class="iconfont">&#xe632;</span>输入城市/景点/游玩主题
     </div>
-    <div class="header-right">
-      {{this.city}}
-      <span class="iconfont arrow-icon">&#xe64a;</span>
-    </div>
+    <router-link to="/city">
+      <div class="header-right">
+        {{this.city}}
+        <span class="iconfont arrow-icon">&#xe64a;</span>
+      </div>
+    </router-link>
   </div>
 </template>
 
@@ -26,8 +28,8 @@ export default {
 @import '~styles/varibles.styl'
 .header
   display: flex
-  height: 0.86rem
-  line-height: 0.86rem
+  height: $headerHeight
+  line-height: $headerHeight
   background: $bgColor
   color: #fff
   .header-left
@@ -50,6 +52,7 @@ export default {
     float: right
     width: 1.24rem
     text-align: center
+    color #fff
     .arrow-icon
       margin-left: -.04rem
       font-size: .24rem
