024e379ff592f7e81a0314a7e846edc3ca5093da
曾志勇 <zengzhiyong@jiaxincloud.com>
2018-07-24 10:18:46 +0800

    利用vuex管理公用city数据

--- src/pages/city/components/Search.vue
+++ src/pages/city/components/Search.vue
@@ -5,7 +5,11 @@
     </div>
     <div class="search-content" v-show="keyword" ref="search">
       <ul>
-        <li class="search-item border-bottom" v-for="(item, index) of list" :key="index">
+        <li
+          class="search-item border-bottom"
+          v-for="(item, index) of list" :key="index"
+          @click="handleCityClick(item.name)"
+        >
           {{item.name}}
         </li>
         <li class="search-item border-bottom" v-show="hasNoData">
@@ -18,6 +22,7 @@
 
 <script>
 import Bscroll from 'better-scroll'
+import { mapMutations } from 'vuex'
 export default {
   name: 'CitySearch',
   props: {
@@ -34,6 +39,13 @@ export default {
     hasNoData () {
       return !this.list.length
     }
+  },
+  methods: {
+    handleCityClick (city) {
+      this.changeCity(city)
+      this.$router.push('/')
+    },
+    ...mapMutations(['changeCity'])
   },
   watch: {
     keyword () {
--- src/pages/home/components/header.vue
+++ src/pages/home/components/header.vue
@@ -16,10 +16,11 @@
 </template>
 
 <script>
+import { mapState } from 'vuex'
 export default {
   name: 'HomeHeader',
-  props: {
-    city: String
+  computed: {
+    ...mapState(['city'])
   }
 }
 </script>
@@ -50,7 +51,8 @@ export default {
     color: #ccc
   .header-right
     float: right
-    width: 1.24rem
+    min-width: 1.04rem
+    padding: 0 .1rem
     text-align: center
     color #fff
     .arrow-icon
